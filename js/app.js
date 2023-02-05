c6156ecbf0a68ee56afa180019829e7b27009670
Sergey51222 <sergejrozumenko512gmail.com>
2023-02-05 19:14:31 +0300

    mi commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+//Menu
+let iconMenu = document.querySelector(".icon-menu");
+if (iconMenu != null) {
+	let delay = 500;
+	let menuBody = document.querySelector(".menu__body");
+	iconMenu.addEventListener("click", function (e) {
+		iconMenu.classList.toggle("_active");
+		menuBody.classList.toggle("_active");
+	});
+};
+function menu_close() {
+	let iconMenu = document.querySelector(".icon-menu");
+	let menuBody = document.querySelector(".menu__body");
+	iconMenu.classList.remove("_active");
+	menuBody.classList.remove("_active");
+}
